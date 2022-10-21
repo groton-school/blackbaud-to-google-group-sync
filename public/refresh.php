aaeddf8315d23f8ae7847f56aea8bb5cf295cdce
Seth Battis <sbattis@groton.org>
2022-10-21 21:48:03 +0000

    Memcached Cloud

--- public/refresh.php
+++ public/refresh.php
@@ -1,36 +1,47 @@
 <?php
 
 use GrotonSchool\OAuth2\Client\Provider\BlackbaudSKY;
-use League\OAuth2\Client\Token\AccessToken;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . "/../vendor/autoload.php";
 
 $sky = new BlackbaudSKY([
     /**
      * Blackbaud subscription access key
      * @link https://developer.blackbaud.com/subscriptions/
      */
-    BlackbaudSKY::ACCESS_KEY => getenv('BLACKBAUD_ACCESS_KEY'),
+    BlackbaudSKY::ACCESS_KEY => getenv("BLACKBAUD_ACCESS_KEY"),
 
     /**
      * OAuth 2.0 App Credentials
      * @link https://developer.blackbaud.com/apps/
      */
     // The client ID assigned to you by the provider
-    'clientId' => getenv('OAUTH_CLIENT_ID'),
+    "clientId" => getenv("BLACKBAUD_CLIENT_ID"),
     // The client password assigned to you by the provider
-    'clientSecret' => getenv('OAUTH_CLIENT_SECRET')
+    "clientSecret" => getenv("BLACKBAUD_CLIENT_SECRET"),
 ]);
 
-$existingAccessToken = new AccessToken($_POST); // get access token from your data store
+$mc = new Memcached();
+$mc->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
+$mc->addServers(
+    array_map(function ($server) {
+        return explode(":", $server, 2);
+    }, explode(",", $_ENV["MEMCACHEDCLOUD_SERVERS"]))
+);
+$mc->setSaslAuthData(
+    $_ENV["MEMCACHEDCLOUD_USERNAME"],
+    $_ENV["MEMCACHEDCLOUD_PASSWORD"]
+);
+
+$existingAccessToken = $mc->get("token"); // get access token from your data store
 
 // FIXME normally we'd test $existingAccessToken->hasExpired() before refreshing
-$newAccessToken = $sky->getAccessToken('refresh_token', [
-    'refresh_token' => $existingAccessToken->getRefreshToken()
+$newAccessToken = $sky->getAccessToken("refresh_token", [
+    "refresh_token" => $existingAccessToken->getRefreshToken(),
 ]);
+$mc->set("token", $newAccessToken);
 
 // Purge old access token and store new access token to your data store.
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -41,15 +52,12 @@ $newAccessToken = $sky->getAccessToken('refresh_token', [
     <h1>Refresh Token</h1>
     <p>Requested an access token using <code>refresh</code> flow.</p>
     <h3>Access Token</h3>
-    <pre lang="json"><?= json_encode($newAccessToken, JSON_PRETTY_PRINT) ?></pre>
+    <p><?php if ($newAccessToken) {
+        echo "Stored in Memcached.";
+    } else {
+        echo "Failed.";
+    } ?></p>
     <form method="post" action="refresh.php">
-        <?php
-        foreach ($newAccessToken->jsonSerialize() as $param => $value) {
-            echo <<<EOT
-        <input name="{$param}" value="$value" type="hidden" />
-EOT;
-        }
-        ?>
         <button type="submit">Refresh Token</button>
     </form>
     </body>
--- public/token.php
+++ public/token.php
@@ -2,66 +2,78 @@
 
 use GrotonSchool\OAuth2\Client\Provider\BlackbaudSKY;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . "/../vendor/autoload.php";
 
 session_start();
 
 $sky = new BlackbaudSKY([
-
     /**
      * Blackbaud subscription access key
      * @link https://developer.blackbaud.com/subscriptions/
      */
-    BlackbaudSKY::ACCESS_KEY => getenv('BLACKBAUD_ACCESS_KEY'),
+    BlackbaudSKY::ACCESS_KEY => getenv("BLACKBAUD_ACCESS_KEY"),
 
     /**
      * OAuth 2.0 App Credentials
      * @link https://developer.blackbaud.com/apps/
      */
     // The client ID assigned to you by the provider
-    'clientId' => getenv('OAUTH_CLIENT_ID'),
+    "clientId" => getenv("BLACKBAUD_CLIENT_ID"),
     // The client password assigned to you by the provider
-    'clientSecret' => getenv('OAUTH_CLIENT_SECRET'),
+    "clientSecret" => getenv("BLACKBAUD_CLIENT_SECRET"),
     // Redirect URI registered with the provider
-    'redirectUri' => getenv('OAUTH_REDIRECT_URL')
+    "redirectUri" => getenv("BLACKBAUD_REDIRECT_URL"),
 ]);
 
 // If we don't have an authorization code then get one
-if (!isset($_GET['code'])) {
+if (!isset($_GET["code"])) {
     // Fetch the authorization URL from the provider; this returns the
     // urlAuthorize option and generates and applies any necessary parameters
     // (e.g. state).
     $authorizationUrl = $sky->getAuthorizationUrl();
 
     // Get the state generated for you and store it to the session.
-    $_SESSION['oauth2state'] = $sky->getState();
+    $_SESSION["oauth2state"] = $sky->getState();
 
     // Redirect the user to the authorization URL.
-    header('Location: ' . $authorizationUrl);
-    exit;
+    header("Location: " . $authorizationUrl);
+    exit();
 
-// Check given state against previously stored one to mitigate CSRF attack
-} elseif (empty($_GET['state']) || (isset($_SESSION['oauth2state']) && $_GET['state'] !== $_SESSION['oauth2state'])) {
-    if (isset($_SESSION['oauth2state'])) {
-        unset($_SESSION['oauth2state']);
+    // Check given state against previously stored one to mitigate CSRF attack
+} elseif (
+    empty($_GET["state"]) ||
+    (isset($_SESSION["oauth2state"]) &&
+        $_GET["state"] !== $_SESSION["oauth2state"])
+) {
+    if (isset($_SESSION["oauth2state"])) {
+        unset($_SESSION["oauth2state"]);
     }
 
-    exit('Invalid state');
+    exit("Invalid state");
 } else {
     try {
         // Try to get an access token using the authorization code grant.
-        $accessToken = $sky->getAccessToken('authorization_code', [
-            'code' => $_GET['code']
+        $accessToken = $sky->getAccessToken("authorization_code", [
+            "code" => $_GET["code"],
         ]);
 
-        $school = $sky->endpoint('school/v1');
-        $levels = $school->get('levels');
+        $mc = new Memcached();
+        $mc->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
+        $mc->addServers(
+            array_map(function ($server) {
+                return explode(":", $server, 2);
+            }, explode(",", $_ENV["MEMCACHEDCLOUD_SERVERS"]))
+        );
+        $mc->setSaslAuthData(
+            $_ENV["MEMCACHEDCLOUD_USERNAME"],
+            $_ENV["MEMCACHEDCLOUD_PASSWORD"]
+        );
+        $mc->set("token", $accessToken);
     } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
         // Failed to get the access token or user details.
         exit($e->getMessage());
     }
 }
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -69,20 +81,17 @@ if (!isset($_GET['code'])) {
         <title>Request Token</title>
     </head>
     <body>
-    
+
     <h1>Request Token</h1>
     <p>Requested an access token using <code>authorization_code</code> flow.</p>
-    
+
     <h3>Access Token</h3>
-    <pre lang="json"><?= json_encode($accessToken, JSON_PRETTY_PRINT) ?></pre>
+    <p><?php if ($accessToken) {
+        echo "Stored in Memcached Cloud";
+    } else {
+        echo "Failed";
+    } ?></p>
     <form method="post" action="refresh.php">
-        <?php
-        foreach ($accessToken->jsonSerialize() as $param => $value) {
-            echo <<<EOT
-        <input name="{$param}" value="$value" type="hidden" />
-EOT;
-        }
-        ?>
         <button type="submit">Refresh Token</button>
     </form>
 
