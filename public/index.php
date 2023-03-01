9ca9c210d9856342fb7847da105e574326a8f7be
Seth Battis <sbattis@groton.org>
2023-03-01 18:48:19 +0000

    whoopsie

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$endpoint = @parse_url($_SERVER['REQUEST_URI'])['path'];
-if (is_readable(__DIR__ . $endpoint)) {
-    require(__DIR__ . $endpoint);
-} else {
-    http_response_code(404);
-    exit('Not Found');
-}
