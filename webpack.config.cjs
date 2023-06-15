3461eeb61cf92e6cefd10f051b2370118617778c
Seth Battis <sbattis@groton.org>
2023-06-15 18:05:04 -0400

    Update webpack.config.cjs

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ const config = require('@battis/webpack/ts/spa');
 
 module.exports = config({
   root: __dirname,
-  appName: 'Blackbaud to Google Groups Sync',
+  appName: 'Blackbaud-to-Google Group Sync',
   entry: './src/client/index.ts',
   template: 'template',
   build: 'public'
