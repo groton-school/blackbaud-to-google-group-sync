3e0fcfa55abe7831c3784c38f9f239cc08f957fb
Seth Battis <sbattis@groton.org>
2023-07-08 10:06:32 -0400

    pdate dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
-const config = require('@battis/webpack/ts/spa');
-
-module.exports = config({
+module.exports = require('@battis/webpack/ts/spa')({
   root: __dirname,
   appName: 'Blackbaud-to-Google Group Sync',
   entry: './src/client/index.ts',
